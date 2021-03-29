a6a3bfea6f3d77b44bd82eebc2cfff6b32477c2e
Steven Hartley <steven.c.hartley@gmail.com>
2021-03-29 16:05:44 +1300

    Running park goer migrations

---
+++ Migrations/20210329030523_Adding Park Goers.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leashApi.Migrations
+{
+    public partial class AddingParkGoers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ParkItemId",
+                table: "UserData",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserData_ParkItemId",
+                table: "UserData",
+                column: "ParkItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserData_ParkItems_ParkItemId",
+                table: "UserData",
+                column: "ParkItemId",
+                principalTable: "ParkItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserData_ParkItems_ParkItemId",
+                table: "UserData");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserData_ParkItemId",
+                table: "UserData");
+
+            migrationBuilder.DropColumn(
+                name: "ParkItemId",
+                table: "UserData");
+        }
+    }
+}
